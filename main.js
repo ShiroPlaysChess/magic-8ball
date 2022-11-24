68a398b4edadea790a28069ab7bc16996ab0f944
lelouch <93632866+lelouchlammperouge@users.noreply.github.com>
2022-11-23 21:38:28 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+document.getElementById("ask question").addEventListener("click", askQuestion)
+
+let withoutADoubt = "Without a Doubt."
+let asISeeItYes = "As I see it, yes."
+let askAgain = "Concentrate and ask again."
+let dontCountOnIt = "Don't count on it."
+let outLookNotSoGood = "Outlook not so good."
+
+function askQuestion () {
+let randnum = Math.random();
+if (randnum < 0.2) {
+    output.innerHTML = withoutADoubt
+} else if (randnum > 0.2 && randnum < 0.4) {
+    output.innerHTML = asISeeItYes
+} else if (randnum > 0.4 && randnum < 0.6) {
+    output.innerHTML = askAgain
+} else if (randnum > 0.6 && randnum < 0.8) {
+    output.innerHTML = dontCountOnIt
+} else if (randnum > 0.8 && randnum < 1) {
+    output.innerHTML = outLookNotSoGood
+}
+}
